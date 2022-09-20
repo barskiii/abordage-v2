0be82bc8a510556e499895d1747a4c28af8c1d51
Andrija Joksimovic <andrija.joksimovic@yahoo.com>
2022-09-20 17:12:33 +0200

    Working on responsivnes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,8 @@
-
-import Image from "next/image"
+import { AiOutlineMenu } from "react-icons/ai"
 
 function Hero() {
   return (
-    <section className="w-[100vw] h-[100vh] max-w-[100%] flex justify-center">
+    <section className="w-[100vw] h-[100vh] max-w-[100%] flex justify-center transition-all duration-150">
         <div className=" w-[85%] h-[100%]">
             {/* Top section */}
             <div className="w-[100%] h-20 flex items-center justify-between">
@@ -15,27 +14,30 @@ function Hero() {
                 </div>
 
                 {/* Navbar */}
-                <div className="w-[40%] h-[90%] ml-6 flex justify-between items-center">
+                <div className="w-[40%] h-[90%] ml-6 lg:flex justify-between items-center hidden">
                     <a href="" className="font-semibold">Početna</a>
                     <a href="" className="font-semibold text-[#60E1CB]">O nama</a>
                     <a href="" className="font-semibold text-[#60E1CB]">Usluge</a>
                     <a href="" className="font-semibold text-[#60E1CB]">Kontakt</a>
                 </div>
+                <div className="h-[90%] flex justify-center items-center lg:hidden">
+                    <AiOutlineMenu color="#60E1CB" />
+                </div>
             </div>
 
             {/* Main hero flex */}
-            <div className="w-[100%] h-[60%] mt-32 flex">
+            <div className="w-[100%] md:h-[60%] mt-32 flex md:flex-row">
                 {/* Hero - cta */}
-                <div className="w-[50%] h-[100%] flex flex-col space-y-4">
+                <div className="md:w-[50%] md:h-[100%] flex flex-col md:space-y-6">
                     {/* Header */}
-                    <div className="h-[20%] w-[100%] relative">
+                    <div className=" w-[100%] relative">
                         <div className="w-[100px] h-[3px] bg-[#60E1CB] absolute top-0 left-0 mt-[-5px]">
 
                         </div>
-                        <h1 className="text-4xl font-semibold">Za <span className="text-[#60E1CB]">brže</span> i <span className="text-[#60E1CB]">sigurnije</span><br/>poslovanje</h1>
+                        <h1 className="md:text-4xl font-semibold">Za <span className="text-[#60E1CB]">brže</span> i <span className="text-[#60E1CB]">sigurnije</span><br/>poslovanje</h1>
                     </div>
 
-                    <div className="h-[22%] w-[100%] text-justify flex items-center">
+                    <div className="w-[100%] text-justify flex items-center">
                         <p className="block">U vremenu gdje je komunikacija sa klijentima ključ biznisa, a prijetnje vrebaju na svakom online i offline uglu, Abordage Vam pruža priliku da bezbjedno uspostavite svoje online prisustvo, i
 nizom usluga poboljša i ubrza Vaše poslovanje.</p>                        
                     </div>
@@ -52,7 +54,7 @@ nizom usluga poboljša i ubrza Vaše poslovanje.</p>
 
                 {/* Image */}
                 <div className="w-[50%] h-[100%] center justify-center items-center">
-                    <img src="/hero-1.png" alt="Hero" className="h-[100%] w-auto mt-[-28px]"/>
+                    <img src="/hero-1.png" alt="Hero" className="w-[100%] h-auto mt-[-28px]"/>
                 </div>
             </div>
         </div>
